ddbe5cad88672fa3f2422518f55986b1c52b17bf
Vitalii Honchar <weaxme@gmail.com>
2022-12-13 07:35:48 -0500

    Added simple tcp server

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"net"
+)
+
+const port = 8080
 
 func main() {
-	fmt.Println("Hello, World")
+	log.Printf("Starting TCP server at port %v\n", port)
+
+	ln, err := net.Listen("tcp", fmt.Sprintf(":%v", port))
+	if err != nil {
+		log.Fatalln("Can't start server", err)
+	}
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			continue
+		}
+		handle(conn)
+	}
+}
+
+func handle(conn net.Conn) {
+	defer conn.Close()
+
+	io.WriteString(conn, "I see you connected\n")
 }
