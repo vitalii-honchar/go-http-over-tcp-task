574bf84ec9614fb611da4e9874320f5f9ff1d17b
Vitalii Honchar <weaxme@gmail.com>
2022-12-13 08:58:36 -0500

    Return html response

--- main.go
+++ main.go
@@ -9,6 +9,18 @@ import (
 
 const port = 8080
 
+const responseBodyTemplate = `<h1>You made a request</h1> 
+<p>method = %s</p> 
+<p>path = %s</p> 
+<p>version = %s</p>`
+
+const responseTemplate = `HTTP/1.1 200 OK
+Content-Length: %d
+Content-Type: text/html
+
+%s
+`
+
 func main() {
 	s := tcp.NewTcpServer(port)
 	sh := &SimpleHandler{}
@@ -28,27 +40,21 @@ type SimpleHandler struct {
 }
 
 func (sh *SimpleHandler) Handle(in string) (string, bool) {
-	log.Printf("Input data: %v", in)
-
 	if sh.method == "" {
 		parts := strings.Fields(in)
 
 		sh.method = parts[0]
 		sh.path = parts[1]
 		sh.version = parts[2]
+		log.Printf("method = %s, path = %s, version = %s\n", sh.method, sh.path, sh.version)
 	}
 
 	sh.request += "\n" + in
-	// log.Printf("%+v\n", sh)
 
 	if in == "" {
-		body := "I see you connected\n"
-		resp := fmt.Sprintf(`HTTP/1.1 200 OK
-Content-Length: %d
-Content-Type: text/plain
-
-%s`, len(body), body)
-		log.Println("Send response")
+		body := fmt.Sprintf(responseBodyTemplate, sh.method, sh.path, sh.version)
+		resp := fmt.Sprintf(responseTemplate, len(body), body)
+		log.Printf("%s\n", sh.request)
 		return resp, false
 	}
 
