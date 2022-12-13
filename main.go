1385f38e27e188cb3967c9c86b291db757c83cbb
Vitalii Honchar <weaxme@gmail.com>
2022-12-13 09:09:29 -0500

    Added http endpoints

--- main.go
+++ main.go
@@ -21,9 +21,49 @@ Content-Type: text/html
 %s
 `
 
+const indexBodyTemplate = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>GET INDEX</title>
+</head>
+<body>
+	<h1>"GET INDEX"</h1>
+	<a href="/">index</a><br>
+	<a href="/apply">apply</a><br>
+	<h1>You made a request</h1> 
+	<p>method = %s</p> 
+	<p>path = %s</p>
+</body>
+</html>
+`
+
+const applyBodyTemplate = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Apply</title>
+</head>
+<body>
+	<h1>"Apply"</h1>
+	<form method="post" action="/apply">
+		<input type="submit" />
+	</form>
+</body>
+</html>
+`
+
+type pageFactory func(string, string) (string, bool)
+
 func main() {
 	s := tcp.NewTcpServer(port)
 	sh := &SimpleHandler{}
+	sh.addFactory(postApplyPageFactory)
+	sh.addFactory(getApplyPageFactory)
+	sh.addFactory(indexPageFactory)
+
 	s.Add(sh.Handle)
 
 	err := s.Start()
@@ -33,10 +73,11 @@ func main() {
 }
 
 type SimpleHandler struct {
-	request string
-	method  string
-	path    string
-	version string
+	request       string
+	method        string
+	path          string
+	version       string
+	pageFactories []pageFactory
 }
 
 func (sh *SimpleHandler) Handle(in string) (string, bool) {
@@ -52,11 +93,51 @@ func (sh *SimpleHandler) Handle(in string) (string, bool) {
 	sh.request += "\n" + in
 
 	if in == "" {
-		body := fmt.Sprintf(responseBodyTemplate, sh.method, sh.path, sh.version)
+		var body string
+		for _, factory := range sh.pageFactories {
+			b, ok := factory(sh.method, sh.path)
+			if ok {
+				body = b
+				break
+			}
+		}
 		resp := fmt.Sprintf(responseTemplate, len(body), body)
 		log.Printf("%s\n", sh.request)
+		sh.clear()
 		return resp, false
 	}
 
 	return "", true
+}
+
+func (sh *SimpleHandler) addFactory(f pageFactory) {
+	sh.pageFactories = append(sh.pageFactories, f)
+}
+
+func (sh *SimpleHandler) clear() {
+	sh.method = ""
+	sh.path = ""
+	sh.version = ""
+	sh.request = ""
+}
+
+func indexPageFactory(method string, path string) (string, bool) {
+	if method == "GET" && (path == "/" || path == "/index") {
+		return fmt.Sprintf(indexBodyTemplate, method, path), true
+	}
+	return "", false
+}
+
+func getApplyPageFactory(method string, path string) (string, bool) {
+	if method == "GET" && path == "/apply" {
+		return applyBodyTemplate, true
+	}
+	return "", false
+}
+
+func postApplyPageFactory(method string, path string) (string, bool) {
+	if method == "POST" && path == "/apply" {
+		return "You did it!", true
+	}
+	return "", false
 }
